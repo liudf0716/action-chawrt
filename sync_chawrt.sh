07a13a5a201216ccc070328ae0853c75c5c20b17
Dengfeng Liu <liudf0716@gmail.com>
2024-11-01 18:07:32 +0800

    feat: add support for syncing chawrt branches in sync script
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- sync_chawrt.sh
+++ sync_chawrt.sh
@@ -7,6 +7,12 @@ REPOS=(
     "https://github.com/openwrt/luci.git https://github.com/liudf0716/luci.git luci master"
 )
 
+CHAWRT_BRANCH=(
+    "openwrt chawrt/main main"
+    "packages chawrt/master master"
+    "luci chawrt/master master"
+)
+
 # Handle interruptions gracefully
 trap 'echo "Script interrupted."; exit 1' SIGINT
 
@@ -25,7 +31,7 @@ sync_repo() {
         git clone "$FORK_REPO" "$REPO_DIR" || { echo "Failed to clone $REPO_DIR."; exit 1; }
     fi
 
-    cd "$REPO_DIR"
+    cd "$REPO_DIR" || { echo "Failed to change directory to $REPO_DIR."; exit 1; }
 
     # Check if the upstream remote already exists
     if git remote | grep -q upstream; then
@@ -66,7 +72,56 @@ sync_repo() {
     echo "Successfully synced $REPO_DIR."
 }
 
+# Function to sync a chawrt branch
+sync_chawrt_branch() {
+    local REPO_DIR="$1"
+    local CHAWRT_BRANCH="$2"
+    local MAIN_BRANCH="$3"
+
+    if [ ! -d "$REPO_DIR" ]; then
+        echo "Repository $REPO_DIR does not exist locally. Skipping..."
+        return
+    fi
+
+    cd "$REPO_DIR" || { echo "Failed to change directory to $REPO_DIR."; exit 1; }
+
+    # Checkout to CHAWRT_BRANCH
+    echo "Switching to branch $CHAWRT_BRANCH..."
+    git checkout "$CHAWRT_BRANCH" || { echo "Failed to checkout to $CHAWRT_BRANCH."; exit 1; }
+
+    # Rebase to MAIN_BRANCH
+    echo "Rebasing your fork's $CHAWRT_BRANCH branch onto upstream's $MAIN_BRANCH branch..."
+    git rebase "$MAIN_BRANCH" || { 
+        echo "Rebase failed. Resolve conflicts and run 'git rebase --continue'."; 
+        exit 1; 
+    }
+
+    # Push to remote
+    echo "Pushing rebased changes to your fork's repository..."
+    REPO_PATH="liudf0716/$REPO_DIR"
+    echo "REPO_PATH: $REPO_PATH"
+    git push -f "https://${GH_TOKEN}@github.com/${REPO_PATH}" "$CHAWRT_BRANCH" || { 
+        echo "Failed to push changes to $REPO_DIR."; 
+        exit 1; 
+    }
+
+    cd ..
+}
+
+# Ensure GH_TOKEN is set
+if [ -z "$GH_TOKEN" ]; then
+    echo "GH_TOKEN is not set. Please set the GitHub token and try again."
+    exit 1
+fi
+
 # Iterate over all repositories and sync them
 for REPO_INFO in "${REPOS[@]}"; do
     sync_repo $REPO_INFO
+done
+echo "All repositories have been successfully synced."
+
+# Sync chawrt branches
+echo "Rebasing chawrt branches..."
+for CHARWRT_BRANCH_INFO in "${CHAWRT_BRANCH[@]}"; do
+    sync_chawrt_branch $CHARWRT_BRANCH_INFO
 done
